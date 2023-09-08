cafefa1341692ade4f864e1187e066ecd8a30ee1
Dinesh Mahesh Lalwani <mr.d.m.lalwani@gmail.com>
2023-09-08 14:51:39 +0530

    Episode 3 Igniting the App

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading2 = React.createElement("h1" ,{className:'greeting'},"Namaste Everyone from React!!");
-const heading3 = React.createElement("h2" ,{className:'greeting'},"Namaste 2 Everyone from React!!");
+const Title = function(){     // Normal JS Function
+    <h1>
+        Good Morning!!
+    </h1>
+};
+const heading2 = (      // React element/ normal variable
+    <h1>
+        Namaste from JSX!!
+    </h1>
+);
+const HeaderComponent = () => {         // Functional Component
+    return(
+        <div>
+            <Title />  {/*Title()*/}
+            {heading2}
+            <h2>Namaste from functional component!!</h2>
+        </div>
+    );
+};
 // We can have only one root and one render method throughtout the app
-const container = React.createElement("div",{className: 'container', id: 'containerID'},[heading2,heading3]);
 const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 // passing a react element into the root
-root2.render(container);
+// root2.render(heading2);
+root2.render(<HeaderComponent />);
